39ab8c3a2e00051fa75de494840e02b1d1e03a90
sudheer chowdari kodali <45354847+sudheerkodali@users.noreply.github.com>
2023-11-27 23:13:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,3 +38,74 @@ this.items[this.items.length - 1];
 this.items.length === 0;
   }
 }
+
+
+
+# Lists and its types
+class
+ 
+Node
+ 
+{
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class
+ 
+LinkedList
+ 
+{
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insertAtHead(value) {
+    const newNode = new Node(value);
+
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  insertAtTail(value) {
+    const newNode = new Node(value);
+
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  removeFromHead() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    const removedNode = this.head;
+    this.head = removedNode.next;
+
+    if (this.head === null) {
+      this.tail = null;
+    }
+
+    return removedNode.value;
+  }
+
+  removeFromTail() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    let previousNode = null;
+    let currentNode = this.head
