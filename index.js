1d421e29ad4e8a225dd45cdc975f45bac8fc3772
sudheer chowdari kodali <45354847+sudheerkodali@users.noreply.github.com>
2023-11-27 13:12:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+# Basic structure of DSA, USING STACK with PUSH, POP items
+class Stack {
+  constructor() {
+    this.items = [];
+  }
 
+  push(item) {
+    this.items.push(item);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return
+ 
+undefined;
+    }
+
+    return
+ 
+this.items.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return
+ 
+undefined;
+    }
+
+    return
+ 
+this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return
+ 
+this.items.length === 0;
+  }
+}
