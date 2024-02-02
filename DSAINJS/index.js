a65f8232e1383e85351e7ce7ff0ad8b73c653063
sudheer chowdari kodali <45354847+sudheerkodali@users.noreply.github.com>
2024-02-02 16:34:51 +0530

    Update index.js

--- DSAINJS/index.js
+++ DSAINJS/index.js
@@ -1,1 +1,14 @@
+const ResizableArray = require('./arrays/ResizableArray');
+
+arr = new ResizableArray();
+
+arr.push(10);
+arr.push(20);
+arr.push(30);
+arr.push(1);
+arr.push(40);
+
+for (let i = o; i < arr.size(); i++) {
+    console.log(arr.get(1));
+}
 
