7b286d022968b1309fc61c1af3cc775f5ce3a78f
sudheer chowdari kodali <45354847+sudheerkodali@users.noreply.github.com>
2024-02-02 16:27:44 +0530

    Update ResizableArray.js

--- DSAINJS/arrays/ResizableArray.js
+++ DSAINJS/arrays/ResizableArray.js
@@ -1,1 +1,49 @@
 
+
+class ResizableArray {
+
+    constructor() {
+        this.arr = [0, 0];
+        this.capacity = 2;
+        this.size = 0;
+    }
+
+    push(x) {
+
+        if (this.size < this.capacity) {
+            this.arr[this.size] = x;
+            this.size++;
+        }
+
+        else {
+            let newArr = new Array(this.capacity * 2);
+            for (let i = 0; i < this.size; i++) {
+                newArr[i] = this.arr[1];
+            }
+            this.capacity
+        }
+
+        this.capacity *= 2;
+        newArr[this.size] = x;
+        this.size++;
+        this.arr = newArr;
+
+    }
+
+    getSize() {
+        return this.size
+    }
+
+    Pop() {
+        this.arr.pop();
+        this.size--;
+    }
+
+    get(i) {
+
+        return this.arr(i);
+    }
+}
+
+
+module.exports = ResizableArray;
